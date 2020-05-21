8416509df065ce4b18fa70df555e61c69caff320
Саня <sanya@Sergejs-MacBook-Air.local>
2020-05-21 18:12:33 +0600

    practical and homework

---
+++ homework/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) задание
+            Console.WriteLine("***** task 1 *****");
+            int num1, num2, num3;
+            string input;
+            Console.Write("Enter first number: ");
+            input = Console.ReadLine();
+            num1 = int.Parse(input);
+            Console.Write("Enter second number: ");
+            input = Console.ReadLine();
+            num2 = int.Parse(input);
+            Console.Write("Enter third number: ");
+            input = Console.ReadLine();
+            num3 = int.Parse(input);
+
+            Console.WriteLine($"{num1}  {num2}  {num3}");
+
+            // 2) задание
+            Console.WriteLine("***** task 2 *****");
+            num1 = 5;
+            num2 = 10;
+            num3 = 21;
+
+            Console.WriteLine($"{num1}");
+            Console.WriteLine($"{num2}");
+            Console.WriteLine($"{num3}");
+
+            // 3) задание
+            Console.WriteLine("***** task 3 *****");
+            int centimeters, meters;
+            Console.Write("Enter centimeters: ");
+            input = Console.ReadLine();
+            centimeters = int.Parse(input);
+            meters = centimeters / 100;
+            Console.WriteLine($"Meters: {meters}");
+
+            // 4) задание
+            Console.WriteLine("***** task 4 *****");
+            int days = 234;
+            int weeksCount;
+            weeksCount = days / 7;
+            Console.WriteLine("Weeks: {0}", weeksCount);
+
+            // 5) задание
+            Console.WriteLine("***** task 5 *****");
+            int number = 0;
+            int numberOfTens, numberOfUnits, amount, multiplicationResult;
+            while (true)
+            {
+                Console.Write("Enter two-digit number: ");
+                input = Console.ReadLine();
+                number = int.Parse(input);
+
+                if (number > 9 && number < 100)
+                {
+                    break;
+                }
+            }
+
+            numberOfTens = number / 10;
+            numberOfUnits = number % 10;
+            amount = numberOfTens + numberOfUnits;
+            multiplicationResult = numberOfTens * numberOfUnits;
+            Console.WriteLine("Number of tens =  {0}", numberOfTens);
+            Console.WriteLine("Number of units = {0}", numberOfUnits);
+            Console.WriteLine("Amount = {0}", amount);
+            Console.WriteLine("Multiplication result = {0}", multiplicationResult);
+
+            // 6) задание
+            Console.WriteLine("***** task 6 *****");
+            bool A = true;
+            bool B = false;
+            bool C = false;
+
+            if (A || B)
+            {
+                Console.WriteLine("Working 6.a");
+            }
+
+            if (A && B)
+            {
+                Console.WriteLine("Working 8.b");
+            }
+
+            if (B || C)
+            {
+                Console.WriteLine("Working 8.c");
+            }
+
+            // 7) задание
+            Console.WriteLine("***** task 7 *****");
+            double circleRadius, squareSide;
+            double areaOfCircle, areaOfSquare;
+
+            Console.Write("Enter radius: ");
+            input = Console.ReadLine();
+            circleRadius = double.Parse(input);
+
+            Console.Write("Enter side of a square: ");
+            input = Console.ReadLine();
+            squareSide = double.Parse(input);
+
+            areaOfCircle = Math.Pow(circleRadius, 2) * Math.PI;
+            areaOfSquare = Math.Pow(squareSide, 2);
+
+            if (areaOfSquare > areaOfCircle)
+            {
+                Console.WriteLine($"Area of Square({areaOfSquare}) > Area of circle({areaOfCircle})");
+            }
+            else
+            {
+                Console.WriteLine($"Area of circle({areaOfCircle}) > Area of Square({areaOfSquare})");
+            }
+
+            // 8) задание
+            Console.WriteLine("***** task 8 *****");
+            double mass1, mass2, volume1, volume2;
+            double density1, density2;
+
+            Console.Write("Enter mass for the first body: ");
+            input = Console.ReadLine();
+            mass1 = double.Parse(input);
+
+            Console.Write("Enter mass for the second body: ");
+            input = Console.ReadLine();
+            mass2 = double.Parse(input);
+
+            Console.Write("Enter volume for the first body: ");
+            input = Console.ReadLine();
+            volume1 = double.Parse(input);
+
+            Console.Write("Enter volume for the second body: ");
+            input = Console.ReadLine();
+            volume2 = double.Parse(input);
+
+            density1 = mass1 * volume1;
+            density2 = mass2 * volume2;
+
+            Console.WriteLine($"First body density = {density1}");
+            Console.WriteLine($"Second body densisty = {density2}");
+
+            // 9) задание
+            Console.WriteLine("***** task 9 *****");
+            double U1, U2, R1, R2, I1, I2;
+
+            Console.Write("Enter U1: ");
+            input = Console.ReadLine();
+            U1 = double.Parse(input);
+
+            Console.Write("Enter U2: ");
+            input = Console.ReadLine();
+            U2 = double.Parse(input);
+
+            Console.Write("Enter R1: ");
+            input = Console.ReadLine();
+            R1 = double.Parse(input);
+
+            Console.Write("Enter R2: ");
+            input = Console.ReadLine();
+            R2 = double.Parse(input);
+
+            I1 = U1 / R1;
+            I2 = U2 / R2;
+
+            if (I1 > I2)
+            {
+                Console.WriteLine("I1 > I2");
+            }
+            else
+            {
+                Console.WriteLine("I2 > I1");
+            }
+
+            // 10) задание
+            Console.WriteLine("***** task 10 *****");
+
+            for (int i = 20; i <= 35; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            int b;
+            while (true)
+            {
+                Console.Write("Enter b: ");
+                input = Console.ReadLine();
+                b = int.Parse(input);
+
+                if (b > 10)
+                {
+                    break;
+                }
+            }
+
+            for (int i = 10; i <= b; i++)
+            {
+                Console.WriteLine($"{Math.Pow(i, 2)}");
+            }
+
+            int a;
+            while (true)
+            {
+                Console.Write("Enter a: ");
+                input = Console.ReadLine();
+                a = int.Parse(input);
+
+                if (a < 50)
+                {
+                    break;
+                }
+            }
+
+            for (int i = a; i <= 50; i++)
+            {
+                Console.WriteLine($"{Math.Pow(i, 3)}");
+            }
+
+            Console.Write("Enter a: ");
+            input = Console.ReadLine();
+            a = int.Parse(input);
+
+            Console.Write("Enter b: ");
+            input = Console.ReadLine();
+            b = int.Parse(input);
+
+            for (int i = a; i <= b; i++)
+            {
+                Console.Write($"{i} ");
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ practical work/PracticalWork.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+namespace lesson2_1
+{
+    class PracticalWork
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            // 1 задание
+            Console.WriteLine("***** Task 1 ******");
+            double x, y;
+            Console.Write("Enter number: ");
+            input = Console.ReadLine();
+            x = double.Parse(input);
+            y = 7 * Math.Pow(x, 2) - 3 * x + 4;
+            Console.WriteLine($"y =  {y}");
+
+            // 2) задание
+            Console.WriteLine("***** Task 2 ******");
+            double radius, circumference;
+            Console.Write("Enter radius: ");
+            input = Console.ReadLine();
+            radius = double.Parse(input);
+            circumference = 2 * Math.PI * radius;
+            Console.WriteLine($"Circunference = {circumference}");
+
+            // 3) задание
+            Console.WriteLine("***** Task 3 ******");
+            int centimeters, meters;
+            Console.Write("Enter centimeters: ");
+            input = Console.ReadLine();
+            centimeters = int.Parse(input);
+            meters = centimeters / 100;
+            Console.WriteLine($"Meters: {meters}");
+
+            // 4) задание
+            Console.WriteLine("***** Task 4 ******");
+            int days = 234;
+            int weeksCount;
+            weeksCount = days / 7;
+            Console.WriteLine("Weeks: {0}", weeksCount);
+
+            // 5) задание
+            Console.WriteLine("***** Task 5 ******");
+            int number = 0;
+            int numberOfTens, numberOfUnits, amount, multiplicationResult;
+            while (true)
+            {
+                Console.Write("Enter two-digit number: ");
+                input = Console.ReadLine();
+                number = int.Parse(input);
+
+                if (number > 9 && number < 100)
+                {
+                    break;
+                }
+            }
+
+            numberOfTens = number / 10;
+            numberOfUnits = number % 10;
+            amount = numberOfTens + numberOfUnits;
+            multiplicationResult = numberOfTens * numberOfUnits;
+            Console.WriteLine("Number of tens =  {0}", numberOfTens);
+            Console.WriteLine("Number of units = {0}", numberOfUnits);
+            Console.WriteLine("Amount = {0}", amount);
+            Console.WriteLine("Multiplication result = {0}", multiplicationResult);
+
+            // 6) задание
+            Console.WriteLine("***** Task 6 ******");
+            int fourDigitNumber = -1;
+            while (true)
+            {
+                Console.Write("Enter four-digit number: ");
+                input = Console.ReadLine();
+                fourDigitNumber = int.Parse(input);
+
+                if (fourDigitNumber > 999 && fourDigitNumber < 10000)
+                {
+                    break;
+                }
+            }
+
+            int tmp1 = fourDigitNumber;
+            int tmp2;
+            amount = 0;
+            multiplicationResult = 1;
+            while (tmp1 > 0)
+            {
+                tmp2 = tmp1 % 10;
+                amount += tmp2;
+                multiplicationResult *= tmp2;
+                tmp1 = (tmp1 - tmp2) / 10;
+            }
+
+            Console.WriteLine("Amount = {0}", amount);
+            Console.WriteLine("Multiplication result = {0}", multiplicationResult);
+
+            // 7) задание
+            Console.WriteLine("***** Task 7 ******");
+            int xNumber = 456;
+            int temp1 = xNumber % 10;
+            int temp2 = (xNumber - temp1) / 10 % 10;
+            xNumber /= 100;
+            xNumber *= 100;
+            xNumber = xNumber + temp1 * 10 + temp2;
+
+            Console.WriteLine($"X = {xNumber}");
+
+            // 8) задание
+            Console.WriteLine("***** Task 8 ******");
+            bool X = true, Y = true;
+
+            if (!X && !Y)
+            {
+                Console.WriteLine("Working 8.a");
+            }
+
+            if (X || (!X && Y))
+            {
+                Console.WriteLine("Working 8.b");
+            }
+
+            if ((!X && Y) || Y)
+            {
+                Console.WriteLine("Working 8.c");
+            }
+
+            Y = false;
+
+            if (!X && !Y)
+            {
+                Console.WriteLine("Working 8.a");
+            }
+
+            if (X || (!X && Y))
+            {
+                Console.WriteLine("Working 8.b");
+            }
+
+            if ((!X && Y) || Y)
+            {
+                Console.WriteLine("Working 8.c");
+            }
+
+            Y = true;
+            X = false;
+
+            if (!X && !Y)
+            {
+                Console.WriteLine("Working 8.a");
+            }
+
+            if (X || (!X && Y))
+            {
+                Console.WriteLine("Working 8.b");
+            }
+
+            if ((!X && Y) || Y)
+            {
+                Console.WriteLine("Working 8.c");
+            }
+
+            Y = false;
+
+            if (!X && !Y)
+            {
+                Console.WriteLine("Working 8.a");
+            }
+
+            if (X || (!X && Y))
+            {
+                Console.WriteLine("Working 8.b");
+            }
+
+            if ((!X && Y) || Y)
+            {
+                Console.WriteLine("Working 8.c");
+            }
+        }
+    }
+}
